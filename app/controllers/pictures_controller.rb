dfe8b0458fd588ac1f1dec2125bb56976e9a46c0
maryfranks <maryfranks@gmail.com>
2017-07-18 17:12:08 -0400

    adds delete feature

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -48,4 +48,10 @@ class PicturesController < ApplicationController
     end
   end
 
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to "/pictures"
+  end
+
 end
