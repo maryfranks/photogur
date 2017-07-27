1c626ec43b45f74890024a51ff5fde0fe2fef2bf
maryfranks <maryfranks@gmail.com>
2017-07-27 13:14:53 -0400

    displays error messages when creating new picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,9 @@
 class Picture < ApplicationRecord
 
+  validates :artist, presence: true
+  validates :title, length: { in: 3..20 }
+  validates :url, presence: true, uniqueness: true
+
   def self.newest_first
     Picture.order("created_at DESC")
   end
