60e973bd5f35e45b8e91033c980fb5662758d35d
maryfranks <maryfranks@gmail.com>
2017-07-31 18:06:28 -0400

    adds users controller and new user form with functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      email: params[:user][:email],
+      password: params[:user][:password],
+      password_confirmation: params[:user][:password_confirmation]
+    )
+
+    if @user.save
+      # session[:user_id] = @user.id
+      flash[:notice] = "Welcome!"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pictures#index'
   resources :pictures
-  # get 'pictures' => 'pictures#index'
-  # post 'pictures' => 'pictures#create'
-  # get 'pictures/new' => 'pictures#new'
-  # get 'pictures/:id' => 'pictures#show'
-  # get 'pictures/:id/edit' => "pictures#edit"
-  # patch 'pictures/:id' => "pictures#update"
-  # delete 'pictures/:id' => 'pictures#destroy'
+  resource :user
 end
