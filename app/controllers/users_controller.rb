5bca13f6b2855dab25878bbbaefaec7e6544cb05
maryfranks <maryfranks@gmail.com>
2017-08-23 17:24:51 -0400

    implement strong params in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(
-      email: params[:user][:email],
-      password: params[:user][:password],
-      password_confirmation: params[:user][:password_confirmation]
-    )
+    @user = User.new(user_params)
 
     if @user.save
       session[:user_id] = @user.id
@@ -20,4 +16,10 @@ class UsersController < ApplicationController
     end
   end
 
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:email, :password, :password_confirmation)
 end
